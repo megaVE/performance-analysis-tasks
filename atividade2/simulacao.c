162f7b63d61394788837096eab84ab718fe56fc3
megaVE <vinicius.honorio@sou.unifal-mg.edu.br>
2023-12-08 16:39:45 -0300

    2nd Task Base Complete

--- atividade2/simulacao.c
+++ atividade2/simulacao.c
@@ -4,12 +4,16 @@
 #include <float.h>
 #include <time.h>
 
-#define TAMANHO_PACOTE 188.0 * 8.0  // 188 Bytes
-#define TAMANHO_BANDA 1000000000.0  // 1 Gigabit / segundo
+// Parametros Fixos
+
+#define MAXIMO_ARVORE 100 // Tamanho máximo suportado pela Árvore de Eventos
+
+#define TAMANHO_PACOTE 188.0 * 8.0 // 188 Bytes
+#define TAMANHO_BANDA 1000000000.0 // 1 Gigabit / segundo
 #define TEMPO_SERVICO TAMANHO_PACOTE / TAMANHO_BANDA // Tempo de processamento de cada pacote
-#define TEMPO_LIGACAO 120.0         // 2 minutos
-#define TEMPO_INJECAO 20.0          // 20 segundos
-#define MAXIMO_ARVORE 100            // Tamanho máximo suportado pela árvore
+
+#define TEMPO_INJECAO 20.0 // 20 segundos
+#define TEMPO_CONEXAO 120.0 // 2 minutos
 
 // Evento
 
@@ -42,6 +46,8 @@ struct heap{
     int capacity;
 };
 
+// Funções Auxiliares
+
 void heapify(struct heap * heap, int index){
     int left = index * 2 + 1;
     int right = index * 2 + 2;
@@ -73,6 +79,8 @@ void organiza_heap(struct heap * heap, int index){
         organiza_heap(heap, parent);
     }
 }
+
+// Funções Heap
 
 struct heap * cria_heap(int capacity){
     struct heap * newHeap = (struct heap*)malloc(sizeof(struct heap));
@@ -126,7 +134,6 @@ void imprime_heap(struct heap * heap){
 
 typedef struct {
     double media_chegada;
-    double media_servico;
     double tempo_simulacao;
 } parametros;
 
@@ -134,12 +141,6 @@ void le_parametros(parametros * params){
     printf("Informe o tempo medio entre clientes (s): ");
     scanf("%lF", &params->media_chegada);
     params->media_chegada = 1.0/params->media_chegada;
-
-    // printf("Informe o tempo medio de servico (s): ");
-    // scanf("%lF", &params->media_servico);
-    // params->media_servico = 1.0/params->media_servico;
-
-    params->media_servico = TEMPO_LIGACAO;
 
     printf("Informe o tempo a ser simulado (s): ");
     scanf("%lF", &params->tempo_simulacao);
@@ -167,53 +168,7 @@ double uniforme() {
 	return (1.0 - u);
 }
 
-void inicia_arvore(struct heap ** arvore){ // Não funciona, fazer no próprio código
-    // arvore = cria_heap(MAXIMO_ARVORE);
-    // Primeira Conexão
-    // Primeira Coleta
-}
-
 int main(){
-    // Área de Teste
-    puts("Inicio do teste");
-
-    struct heap * arvore_de_eventos = cria_heap(MAXIMO_ARVORE);
-    printf("Heap Teste: Size = %d, Capacity = %d\n", arvore_de_eventos->size, arvore_de_eventos->capacity);
-
-    struct evento novo_evento = cria_evento('d', 10.0, 0.0);
-    printf("Evento Teste: [%c] %lF - %lF\n", novo_evento.tipo_evento, novo_evento.tempo_evento, novo_evento.tempo_limite);
-
-    insere_heap(arvore_de_eventos, novo_evento);
-    printf("Heap Apos Insercao: Size = %d, Capacity = %d\n", arvore_de_eventos->size, arvore_de_eventos->capacity);
-    imprime_heap(arvore_de_eventos);
-
-    novo_evento = cria_evento('a', 11.0, 0.0);
-    printf("Evento Teste: [%c] %lF - %lF\n", novo_evento.tipo_evento, novo_evento.tempo_evento, novo_evento.tempo_limite);
-    insere_heap(arvore_de_eventos, novo_evento);
-
-    novo_evento = cria_evento('b', 9.0, 0.0);
-    printf("Evento Teste: [%c] %lF - %lF\n", novo_evento.tipo_evento, novo_evento.tempo_evento, novo_evento.tempo_limite);
-    insere_heap(arvore_de_eventos, novo_evento);
-
-    imprime_heap(arvore_de_eventos);
-
-    puts("Extrair");
-    extrai_heap(arvore_de_eventos);
-    imprime_heap(arvore_de_eventos);
-
-    puts("Extrair");
-    extrai_heap(arvore_de_eventos);
-    imprime_heap(arvore_de_eventos);
-
-    puts("Extrair");
-    extrai_heap(arvore_de_eventos);
-    imprime_heap(arvore_de_eventos);
-
-    puts("Fim de teste");
-    return 0;
-}
-
-/*
     // RNG
 
     int semente = time(NULL);
@@ -224,21 +179,28 @@ int main(){
     parametros params;
     le_parametros(&params);
 
+    // Arvore de Eventos
 
-    // Eventos
+    // Árvore Heap de Eventos
+    struct evento evento_atual;
+    struct heap * arvore_de_eventos = cria_heap(MAXIMO_ARVORE);
 
-    struct heap * arvore_de_eventos;
-    inicializa_arvore(arvore_de_eventos);
+    // Primeira Coleta
+    evento_atual = cria_evento('c', 0.0, 0.0);
+    insere_heap(arvore_de_eventos, evento_atual);
+
+    // Primeira Conexão
+    double tempo_chegada = (-1.0 / params.media_chegada) * log(uniforme());
+    evento_atual = cria_evento('n', tempo_chegada, 0.0);
+    insere_heap(arvore_de_eventos, evento_atual);
 
     // Controle de Simulação
     
     double tempo_decorrido = 0.0;
-    // [RECALCULAR] double tempo_chegada = (-1.0/params.media_chegada) * log(uniforme());
-    // [REFATORAR] double tempo_saida = DBL_MAX;
-    // [REFATORAR] double tempo_analise = DBL_MAX;
+
     unsigned long int fila = 0;
     unsigned long int max_fila = 0;
-
+    
     // Medidas de Interesse
 
     double soma_ocupacao = 0.0;
@@ -246,165 +208,132 @@ int main(){
     // Little
 
     little e_n;
-    little e_w_chegada;
-    little e_w_saida;
     inicia_little(&e_n);
+
+    little e_w_chegada;
     inicia_little(&e_w_chegada);
-    inicia_little(&e_w_saida);
-
-   // Novas Variáveis
-
-   struct evento evento_atual;
-
-    //min_heap --> tempo_pacote_conexoes_ativas
-    //eventos: nova_conexao, conexao_ativa_pacote(heap), tempo_servico, coleta_dados
-    // NOVA CONEXÃO: entra um novo cliente na banda
-    // CONEXÃO ATIVA: tempo em que outro pacote chega e entra na fila
-    // TEMPO SERVIÇO: quando o roteador ficará disponível novamente
-    // COLETA DADOS: coleta das informações de little
-    //min(min(min(nova_coxeao, heap.conexao_ativa_pacote),tempo_servico),coleta_dados)
     
-    while(tempo_decorrido < params.tempo_simulacao){
-       
+    little e_w_saida;
+    inicia_little(&e_w_saida);
+    
+    while(1){
         evento_atual = extrai_heap(arvore_de_eventos);
         tempo_decorrido = evento_atual.tempo_evento;
-        //printf("%lF\n", tempo_decorrido);
+        printf("%lF\n", tempo_decorrido);
+
+        if(tempo_decorrido >= params.tempo_simulacao) break;
 
         switch(evento_atual.tipo_evento){
             case 'n': // Nova Conexão
-                // Insere nova conexão na heap
+                // Insere a nova conexão na heap
+                double tempo_conexao = (-1.0 / TEMPO_CONEXAO) * log(uniforme());
+                evento_atual = cria_evento('p', tempo_decorrido, tempo_decorrido + tempo_conexao);
+                insere_heap(arvore_de_eventos, evento_atual);
+
+                // Define o momento da próxima conexão
+                double tempo_chegada = (-1.0 / params.media_chegada) * log(uniforme());
+                evento_atual = cria_evento('n', tempo_decorrido + tempo_chegada, 0.0);
+                insere_heap(arvore_de_eventos, evento_atual);
+
                 break;
             
             case 's': // Tempo de Serviço
-                // Reduz a fila (caso exista)
-                // Insere o próximo momento de disponibilidade na heap
+                // Reduz a fila
+                fila--;
+
+                if(fila){
+                    // Processa o próximo pacote (fila não vazia)
+                    evento_atual = cria_evento('s', tempo_decorrido + TEMPO_SERVICO, 0.0);
+                    insere_heap(arvore_de_eventos, evento_atual);
+                }
+
+                // Calculos de Little (E[N])
+                e_n.soma_areas += (tempo_decorrido - e_n.tempo_anterior) * e_n.no_eventos;
+                e_n.no_eventos--;
+                e_n.tempo_anterior = tempo_decorrido;
+
+                // Calculos de Little (E[W] Saída)
+                e_w_saida.soma_areas += (tempo_decorrido - e_w_saida.tempo_anterior) * e_w_saida.no_eventos;
+                e_w_saida.no_eventos++;
+                e_w_saida.tempo_anterior = tempo_decorrido;
+
                 break;
             
             case 'p': // Tempo de Pacote
                 // Aumenta a fila
-                // Insere o próximo pacote da conexão na heap (caso a conexão persista)
+                fila++;
+                if(fila > max_fila) max_fila = fila;
+                
+                if(!fila){
+                    // Define o tempo de serviço (fila vazia)
+                    evento_atual = cria_evento('s', tempo_decorrido + TEMPO_SERVICO, 0.0);
+                    insere_heap(arvore_de_eventos, evento_atual);
+                }
+
+                if(tempo_decorrido + TEMPO_INJECAO <= evento_atual.tempo_limite){
+                    // Insere o próximo pacote da conexão na heap (caso a conexão persista)
+                    double tempo_limite = evento_atual.tempo_limite;
+                    evento_atual = cria_evento('p', tempo_decorrido + TEMPO_INJECAO, tempo_limite);
+                    insere_heap(arvore_de_eventos, evento_atual);
+                }
+
+                // Calculos de Little (E[N])
+                e_n.soma_areas += (tempo_decorrido - e_n.tempo_anterior) * e_n.no_eventos;
+                e_n.no_eventos++;
+                e_n.tempo_anterior = tempo_decorrido;
+                
+                // Calculos de Little (E[W] Chegada)
+                e_w_chegada.soma_areas += (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
+                e_w_chegada.no_eventos++;
+                e_w_chegada.tempo_anterior = tempo_decorrido;
+
                 break;
 
             case 'c': // Tempo de Coleta
-                // Completa os calculos de little
+                // Calculos de Little
+                e_n.soma_areas += e_n.no_eventos * (tempo_decorrido - e_n.tempo_anterior);
+                e_n.tempo_anterior = tempo_decorrido;
+
+                e_w_chegada.soma_areas += e_w_chegada.no_eventos * (tempo_decorrido - e_w_chegada.tempo_anterior);
+                e_w_chegada.tempo_anterior = tempo_decorrido;
+                
+                e_w_saida.soma_areas += e_w_saida.no_eventos * (tempo_decorrido - e_w_saida.tempo_anterior);
+                e_w_saida.tempo_anterior = tempo_decorrido;
+
+                double e_n_calculo = e_n.soma_areas / tempo_decorrido;
+                double e_w_calculo = (e_w_chegada.soma_areas - e_w_saida.soma_areas) / e_w_chegada.no_eventos;
+                double lambda = e_w_chegada.no_eventos / tempo_decorrido;
+                
                 // Imprime os resultados
+                printf("%.0lF, %.20lF\n", tempo_decorrido, e_n_calculo - lambda * e_w_calculo);
+
                 // Insere o novo valor de calculo na heap
+                evento_atual = cria_evento('c', tempo_decorrido + 10.0, 0.0);
+                insere_heap(arvore_de_eventos, evento_atual);
+
                 break;
 
             default:
                 puts("Evento invalido!");
                 return 1;
         }
-
-
-
-        // if(tempo_decorrido == tempo_analise){
-        //     // analise
-        //     e_n.soma_areas += e_n.no_eventos * (tempo_decorrido - e_n.tempo_anterior);
-        //     e_n.tempo_anterior = tempo_decorrido;
-
-        //     e_w_chegada.soma_areas += e_w_chegada.no_eventos * (tempo_decorrido - e_w_chegada.tempo_anterior);
-        //     e_w_chegada.tempo_anterior = tempo_decorrido;
-            
-        //     e_w_saida.soma_areas += e_w_saida.no_eventos * (tempo_decorrido - e_w_saida.tempo_anterior);
-        //     e_w_saida.tempo_anterior = tempo_decorrido;
-
-        //     double e_n_calculo = e_n.soma_areas / tempo_decorrido;
-        //     double e_w_calculo = (e_w_chegada.soma_areas - e_w_saida.soma_areas) / e_w_chegada.no_eventos;
-        //     double lambda = e_w_chegada.no_eventos / tempo_decorrido;
-
-        //     // printf("%.0lF, %.20lF\n", tempo_analise, e_n_calculo - lambda * e_w_calculo);
-        //     tempo_analise += 10.0;
-        // } else if(tempo_decorrido == tempo_chegada){
-        //     //chegada
-        //     //a cabeca da fila eh quem esta em atendimento
-        //     if(!fila){
-        //         double tempo_servico =
-        //             (-1.0/params.media_servico)
-        //             * log(uniforme());
-                
-        //         tempo_saida = tempo_decorrido
-        //             + tempo_servico;
-
-        //         soma_ocupacao += tempo_servico;
-        //     }
-        //     fila++;
-        //     max_fila = fila > max_fila?
-        //         fila:max_fila;
-            
-        //     tempo_chegada = tempo_decorrido + 
-        //     (-1.0/params.media_chegada) * log(uniforme());
-
-        //     //calculo little -- E[N]
-        //     e_n.soma_areas += 
-        //         (tempo_decorrido - e_n.tempo_anterior)
-        //         * e_n.no_eventos;
-        //     e_n.no_eventos++;
-        //     e_n.tempo_anterior = tempo_decorrido;
-            
-        //     //calculo little -- E[W] - chegada
-        //     e_w_chegada.soma_areas +=
-        //         (tempo_decorrido - e_w_chegada.tempo_anterior)
-        //         * e_w_chegada.no_eventos;
-        //     e_w_chegada.no_eventos++;
-        //     e_w_chegada.tempo_anterior = tempo_decorrido;
-        // } else if(tempo_decorrido == tempo_saida){
-        //     //saida
-        //     fila--;
-        //     if(fila){
-        //         double tempo_servico =
-        //             (-1.0/params.media_servico)
-        //             * log(uniforme());
-                
-        //         tempo_saida = tempo_decorrido
-        //             + tempo_servico;
-
-        //         soma_ocupacao += tempo_servico;
-        //     }else{
-        //         tempo_saida = DBL_MAX;
-        //     }
-
-        //     //calculo little -- E[N]
-        //     e_n.soma_areas += 
-        //         (tempo_decorrido - e_n.tempo_anterior)
-        //         * e_n.no_eventos;
-        //     e_n.no_eventos--;
-        //     e_n.tempo_anterior = tempo_decorrido;
-
-        //     //calculo little -- E[W] - saida
-        //     e_w_saida.soma_areas +=
-        //         (tempo_decorrido - e_w_saida.tempo_anterior)
-        //         * e_w_saida.no_eventos;
-        //     e_w_saida.no_eventos++;
-        //     e_w_saida.tempo_anterior = tempo_decorrido;
-        // } else{
-        //     puts("Evento invalido!\n");
-        //     return(1);
-        // }
     }
-    // e_w_chegada.soma_areas +=
-    //     (tempo_decorrido - e_w_chegada.tempo_anterior)
-    //     * e_w_chegada.no_eventos;
+    
+    // Calculos Finais
 
-    // e_w_saida.soma_areas +=
-    //     (tempo_decorrido - e_w_saida.tempo_anterior)
-    //     * e_w_saida.no_eventos;
+    e_w_chegada.soma_areas += (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
+    e_w_saida.soma_areas += (tempo_decorrido - e_w_saida.tempo_anterior) * e_w_saida.no_eventos;
 
+    double e_n_calculo = e_n.soma_areas / tempo_decorrido;
+    double e_w_calculo = (e_w_chegada.soma_areas - e_w_saida.soma_areas) / e_w_chegada.no_eventos;
+    double lambda = e_w_chegada.no_eventos / tempo_decorrido;
 
+    printf("Ocupacao: %lF\n", (soma_ocupacao/tempo_decorrido));
+    printf("Tamanho maximo da fila: %ld\n", max_fila);
 
-    // double e_n_calculo = e_n.soma_areas / tempo_decorrido;
-    // double e_w_calculo = (e_w_chegada.soma_areas
-    //     - e_w_saida.soma_areas)
-    //     / e_w_chegada.no_eventos;
-    // double lambda = e_w_chegada.no_eventos / tempo_decorrido;
-
-    // printf("ocupacao: %lF\n", (soma_ocupacao/tempo_decorrido));
-    // // printf("tamanho maximo da fila: %ld\n", max_fila);
-
-    // printf("E[N]: %lF\n", e_n_calculo);    
-    // printf("E[W]: %lF\n", e_w_calculo);
-    // printf("Erro de Little: %.20lF\n", e_n_calculo - lambda * e_w_calculo);
-
+    printf("E[N]: %lF\n", e_n_calculo);    
+    printf("E[W]: %lF\n", e_w_calculo);
+    printf("Erro de Little: %.20lF\n", e_n_calculo - lambda * e_w_calculo);
+        
     return 0;
 }
-*/
